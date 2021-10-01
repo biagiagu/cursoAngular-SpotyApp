bd8e178cafb99970016e3fd1a407ef228b361e4d
Agustin Biagi <agustin.biagi@gmail.com>
2021-09-30 21:24:53 -0300

    .

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,10 +11,28 @@ export class SpotifyService {
 
   }
 
+  getSpotifyToken(){
+    
+    
+    const body : any = {
+      'grant_type':'client_credentials',
+      'idClient' :  '7e7034426bdc4b81abb19d1c95701441',
+      'idSecret' :  '9fe0933f0c9b487e93d1b35b15d4204f'
+      
+     }
+    
+
+    this.http.post('https://accounts.spotify.com/api/token', {body} ).subscribe(data =>{ console.log(data)})
+
+  }
+
   getNewReleases(){
 
+    this.getSpotifyToken()
+    //console.log(token);
+
     const headers= new HttpHeaders({
-      'Authorization': 'Bearer BQAJWRfc15GomV_j8Vr6fl3MOTiZpgJ8icwhH4_721acmBlE9Hz66Ye72YIuUM3eCzYAjGLYgffR8TwIK5A'
+      'Authorization': 'Bearer BQAdnyD6JnfFT8KkYkO52-kyCeiivdlyVJ_MhEkxRDhCdsfoaza-w8-NOs-IYk-1imYLWecEkUM4CJAtLkM'
     })
 
     this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers}).subscribe( data =>{
